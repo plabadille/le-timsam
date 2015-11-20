7f39fd2f13bb60b9183d83cb2893db24ff8157d2
plabadille <plabadille@gmail.com>
2015-11-20 15:36:01 +0100

    arborescence

--- index.php
+++ index.php
@@ -1,15 +1,69 @@
 <?php
 
 $nav = file_get_contents('view/templates/nav.phtml');
-$action = isset($GET['action']) ? $_GET['action'] : 'home';
+$action = isset($_GET['action']) ? $_GET['action'] : 'home';
 
 //function
 
 switch ($action) {
+	//onglet1 accueil
 	case 'home':
-	$title = 'Accueil';
-	$main = file_get_contents('view/templates/home.phtml');
-	break;
+		$title = 'Accueil';
+		$main = file_get_contents('view/pages/home.html');
+		break;
+	//onglet2 services
+	case 'services':
+		$title = 'Les services';
+		$main = file_get_contents('view/pages/services.html');
+		break;
+		//sous-onglet
+		case 'restaurant':
+		$title = 'Le restaurant';
+		$main = file_get_contents('view/pages/restaurant.html');
+		break;
+		case 'cartecadeau':
+		$title = 'Notre carte cadeau';
+		$main = file_get_contents('view/pages/cartecadeau.html');
+		break;
+	//onglet3 produits
+	case 'carte':
+		$title = 'Menu et carte';
+		$main = file_get_contents('view/pages/carte.html');
+		break;
+		//sous-onglet
+		case 'entrees':
+		$title = 'Nos entrées';
+		$main = file_get_contents('view/pages/entrees.html');
+		break;
+		case 'plats':
+		$title = 'Nos plats';
+		$main = file_get_contents('view/pages/plats.html');
+		break;
+		case 'desserts':
+		$title = 'Nos desserts';
+		$main = file_get_contents('view/pages/desserts.html');
+		break;
+	//onglet4 animations
+	case 'event':
+		$title = 'Nos animations';
+		$main = file_get_contents('view/pages/event.html');
+		break;
+		//sous-onglet
+		case 'galerie':
+		$title = 'La galerie';
+		$main = file_get_contents('view/pages/galerie.html');
+		break;
+		case 'agenda':
+		$title = 'Notre agenda';
+		$main = file_get_contents('view/pages/agenda.html');
+		break;
+	//onglet5 contact
+	case 'contact':
+		$title = 'Contact';
+		$main = file_get_contents('view/pages/contact.html');
+		break;
 }
+
+$title .= ' - Restaurant le Timsam';
 
 include('view/base.phtml');
