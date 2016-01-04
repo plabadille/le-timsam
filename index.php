9fc5089f4df43bddb50e17414f5f9eddb6500773
plabadille <plabadille@gmail.com>
2016-01-04 19:18:05 +0100

    version 2 mise en ligne. Epuration menu, pleine page, compatibilité w3c, full plateforme, full browsers

--- index.php
+++ index.php
@@ -6,51 +6,28 @@ $action = isset($_GET['action']) ? $_GET['action'] : 'home';
 //function
 
 switch ($action) {
-	//onglet1 accueil
 	case 'home':
 		$title = 'Accueil';
 		$main = file_get_contents('view/pages/home.html');
 		break;
-	//onglet2 services
-	case 'services':
-		$title = 'Les services';
-		$main = file_get_contents('view/pages/services.html');
-		break;
-		//sous-onglet
-		case 'restaurant':
-		$title = 'Le restaurant';
-		$main = file_get_contents('view/pages/restaurant.html');
-		break;
-		case 'cartecadeau':
-		$title = 'Notre carte cadeau';
-		$main = file_get_contents('view/pages/cartecadeau.html');
-		break;
-	//onglet3 produits
-	case 'carte':
-		$title = 'Menu et carte';
-		$main = file_get_contents('view/pages/carte.html');
-		break;
-		//sous-onglet
-		case 'entrees':
-		$title = 'Nos entrées';
-		$main = file_get_contents('view/pages/entrees.html');
-		break;
-		case 'plats':
-		$title = 'Nos plats';
-		$main = file_get_contents('view/pages/plats.html');
-		break;
-		case 'desserts':
-		$title = 'Nos desserts';
-		$main = file_get_contents('view/pages/desserts.html');
-		break;
-	//onglet4 animations
+
+	case 'restaurant':
+	$title = 'Le restaurant';
+	$main = file_get_contents('view/pages/restaurant.html');
+	break;
+	
+	case 'produits':
+	$title = 'Nos produits';
+	$main = file_get_contents('view/pages/produits.html');
+	break;
+		
 	case 'event':
 		$title = 'Nos animations';
 		$main = file_get_contents('view/pages/event.html');
 		break;
 			//uniquement accessible via event.html
 			case 'article1':
-			$title = 'Exposition Marie Hamel';
+			$title = 'Nos animations';
 			$main = file_get_contents('view/pages/articles/1_mariehamel.html');
 			break;
 			case 'article2':
@@ -77,19 +54,10 @@ switch ($action) {
 			$title = 'Animation culturelle';
 			$main = file_get_contents('view/pages/articles/7_animculturelle.html');
 			break;
-		//sous-onglet
-		case 'galerie':
-		$title = 'La galerie';
-		$main = file_get_contents('view/pages/galerie.html');
-		break;
-		case 'agenda':
-		$title = 'Notre agenda';
-		$main = file_get_contents('view/pages/agenda.html');
-		break;
-	//onglet5 contact
-	case 'contact':
-		$title = 'Contact';
-		$main = file_get_contents('view/pages/contact.html');
+		
+	case 'carte':
+		$title = 'Carte et réservation en ligne';
+		$main = file_get_contents('view/pages/carte.html');
 		break;
 }
 
--- js/script.js
+++ js/script.js
@@ -35,15 +35,6 @@ var ul=document.querySelector('#mainNav #navigation');
 var declencheurMobile=document.querySelector('#mainNav #navigationMobile a');
 var mobile=document.querySelector('#mainNav #navigationMobile');
 
-
-var ul1=document.querySelector('#mainNav #navigation li:nth-child(2) ul');
-var ul2=document.querySelector('#mainNav #navigation li:nth-child(3) ul');
-var ul3=document.querySelector('#mainNav #navigation li:nth-child(4) ul');
-
-var a2=document.querySelector('#mainNav #navigation > li:nth-child(2) a');
-var a3=document.querySelector('#mainNav #navigation > li:nth-child(3) a');
-var a4=document.querySelector('#mainNav #navigation > li:nth-child(4) a');
-
 menu();
 window.addEventListener("resize", menu);
 
@@ -57,39 +48,15 @@ function menu(){
 
 function normalRez(){
 	declencheurMobile.textContent = "Menu  ";
-	a2.textContent = "Services";
-	a3.textContent = "Produits";
-	a4.textContent = "Animations";
-	declencheurMobile.removeEventListener("click", derouler);
-	a2.removeEventListener("click", derouler1, false);
-	a3.removeEventListener('click', derouler2, false);
-	a4.removeEventListener('click', derouler3, false);
-	declencheurMobile.removeEventListener("click", renrouler);
-	a2.removeEventListener("click", renrouler1, false);
-	a3.removeEventListener('click', renrouler2, false);
-	a4.removeEventListener('click', renrouler3, false);
 	mobile.style.display = "none";
 	ul.style.display = "flex";
-	ul1.style.display = "initial";
-	ul2.style.display = "initial";
-	ul3.style.display = "initial";
 }
 
 function basseRez(){
 	ul.style.display = "none";
-	ul1.style.display = "none";
-	ul2.style.display = "none";
-	ul3.style.display = "none";
 	mobile.style.display = "flex";
 	declencheurMobile.textContent = "Menu  ▼";
-	a2.textContent = "Services ▼";
-	a3.textContent = "Produits ▼";
-	a4.textContent = "Animations ▼";
-
 	declencheurMobile.addEventListener("click", derouler, false);
-	a2.addEventListener("click", derouler1, false);
-	a3.addEventListener('click', derouler2, false);
-	a4.addEventListener('click', derouler3, false);
 }
 
 function derouler(ev){
@@ -103,51 +70,6 @@ function renrouler(ev){
 	ev.preventDefault();
 	ul.style.display = "none";
 	declencheurMobile.removeEventListener("click", renrouler);
-	/*a2.textContent = "Services ▼";*/
-	return;
-}
-
-function derouler1(ev){
-	ev.preventDefault();
-	ul1.style.display = "initial";
-	a2.textContent = "Services ▲";
-	a2.addEventListener('click', renrouler1, false);
-	return;
-}
-function renrouler1(ev){
-	ev.preventDefault();
-	ul1.style.display = "none";
-	a2.removeEventListener("click", renrouler1);
-	a2.textContent = "Services ▼";
-	return;
-}
-
-function derouler2(ev){
-	ev.preventDefault();
-	ul2.style.display = "initial";
-	a3.textContent = "Produits ▲";
-	a3.addEventListener('click', renrouler2, false);
-	return;
-}
-function renrouler2(ev){
-	ev.preventDefault();
-	ul2.style.display = "none";
-	a3.removeEventListener("click", renrouler2);
-	a3.textContent = "Produits ▼";
-	return;
-}
-
-function derouler3(ev){
-	ev.preventDefault();
-	ul3.style.display = "initial";
-	a4.textContent = "Animations ▲";
-	a4.addEventListener('click', renrouler3, false);
-	return;
-}
-function renrouler3(ev){
-	ev.preventDefault();
-	ul3.style.display = "none";
-	a4.removeEventListener("click", renrouler3);
-	a4.textContent = "Animations ▼";
+	declencheurMobile.textContent = "Menu  ▼";
 	return;
 }
