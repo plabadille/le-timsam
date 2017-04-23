5bc3a0f918299cf6b05fc4a6d3f488cf6ed091e3
plabadille <plabadille@gmail.com>
2017-04-23 19:04:56 +0200

    refactoring and design page event

--- evenement.php
+++ evenement.php
@@ -49,10 +49,70 @@ switch ($action) {
         $current_page = " Conférence : Secret d\'un chef dans la nature";
         $main = file_get_contents('view/pages/articles/9_secretDunChef.html');
         break;
+    case 'article10':
+        $title = 'Animation culturelle 2';
+        $current_page = " Animation culturelle 2";
+        $main = file_get_contents('view/pages/articles/10_animculturelle.html');
+        break;
+    case 'article11':
+        $title = 'Soirée spéciale : Soirée à la table du chef';
+        $current_page = " Soirée spéciale : Soirée à la table du chef";
+        $main = file_get_contents('view/pages/articles/11_soireeTableChef.html');
+        break;
+    case 'article12':
+        $title = 'Conférence : Secret d\'un chef dans la nature 2ème édition';
+        $current_page = " Conférence : Secret d'un chef dans la nature 2ème édition";
+        $main = file_get_contents('view/pages/articles/12_secretDunChef2.html');
+        break;
+    case 'article13':
+        $title = 'Retour sur la soirée à la table du chef';
+        $current_page = " Retour sur la soirée à la table du chef";
+        $main = file_get_contents('view/pages/articles/13_soireeTableChefDebrief1.html');
+        break;
+    case 'article14':
+        $title = 'Soirée spéciale : Soirée à la table du chef (2ème édition)';
+        $current_page = " Soirée à la table du chef (2ème édition)";
+        $main = file_get_contents('view/pages/articles/14_soireeTableChef2.html');
+        break;
+    case 'article15':
+        $title = 'Retour sur la soirée à la table du chef (2ème édition)';
+        $current_page = " Retour sur la soirée à la table du chef (2ème édition)";
+        $main = file_get_contents('view/pages/articles/15_soireeTableChefDebrief2.html');
+        break;
+    case 'article16':
+        $title = 'Retour sur la soirée à la table du chef (3ème édition)';
+        $current_page = " Retour sur la soirée à la table du chef (3ème édition)";
+        $main = file_get_contents('view/pages/articles/16_soireeTableChefDebrief3.html');
+        break;
+    case 'article17':
+        $title = 'Retour sur la soirée à la table du chef (4ème édition)';
+        $current_page = " Retour sur la soirée à la table du chef (4ème édition)";
+        $main = file_get_contents('view/pages/articles/17_soireeTableChefDebrief4.html');
+        break;
+    case 'article18':
+        $title = 'Retour sur la soirée à la table du chef (5ème édition)';
+        $current_page = " Retour sur la soirée à la table du chef (5ème édition)";
+        $main = file_get_contents('view/pages/articles/18_soireeTableChefDebrief5.html');
+        break;
+    case 'article19':
+        $title = 'Animation culturelle 3';
+        $current_page = " Animation culturelle 3";
+        $main = file_get_contents('view/pages/articles/19_animculturelle.html');
+        break;
+    case 'article20':
+        $title = 'Retour sur la soirée à la table du chef (6ème édition)';
+        $current_page = " Retour sur la soirée à la table du chef (6ème édition)";
+        $main = file_get_contents('view/pages/articles/20_soireeTableChefDebrief6.html');
+        break;
+    case 'article21':
+        $title = 'L\'exposition de Céline Azorin';
+        $current_page = " L'exposition de Céline Azorin";
+        $main = file_get_contents('view/pages/articles/21_expoCecileAzorin.html');
+        break;
 }
 
 if (isset($_GET['action']) && $_GET['action'] != 'event') {
-    array_push($breadcrumb, ["Nos animations", "./event.php"]);
+    array_push($breadcrumb, ["Nos animations", "./evenement.php"]);
 }
 $title .= ' - Restaurant le Timsam';
 
